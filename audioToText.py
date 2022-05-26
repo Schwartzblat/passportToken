58bd5ebdb6239a66558fbf3a28a76f2d0719c28f
Schwartzblat <alon.ponch@gmail.com>
2022-05-26 14:06:39 +0300

    init

---
+++ audioToText.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import sys
+import base64
+import shutil
+
+
+def audio_to_text(path):
+    r = sr.Recognizer()
+    def get_large_audio_transcription():
+
+        sound = AudioSegment.from_mp3(path)
+        chunks = split_on_silence(sound,
+                                  min_silence_len=500,
+                                  silence_thresh=sound.dBFS - 14,
+                                  keep_silence=500,
+                                  )
+        folder_name = os.path.join(os.path.dirname(__file__), "audio-chunks")
+        if not os.path.isdir(folder_name):
+            os.mkdir(folder_name)
+        whole_text = ""
+        for i, audio_chunk in enumerate(chunks, start=1):
+            chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+            audio_chunk.export(chunk_filename, format="wav")
+            with sr.AudioFile(chunk_filename) as source:
+                audio_listened = r.record(source)
+                try:
+                    text = r.recognize_google(audio_listened, language="en")
+                except sr.UnknownValueError as e:
+                    pass
+                else:
+                    text = f"{text} "
+                    whole_text += text
+        return whole_text
+
+    text = get_large_audio_transcription()
+
+    try:
+        shutil.rmtree(os.path.join(os.path.dirname(__file__), "audio-chunks"))
+    except Exception:
+        print(Exception)
+    return text
---
+++ first.py
@@ -0,0 +1,96 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from audioToText import audio_to_text
+import time
+import datetime
+import requests
+import os
+import timeit
+
+start_time = timeit.default_timer()
+
+
+def url_to_text(url: str):
+    res = requests.get(url)
+    with open('temp.mp3', 'wb') as f:
+        f.write(res.content)
+    temp_text = audio_to_text('temp.mp3')
+    os.remove('temp.mp3')
+    return temp_text
+
+
+chrome_options = Options()
+chrome_options.add_argument("start-maximized")
+# chrome_options.add_argument("--headless")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s, options=chrome_options)
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+
+driver.set_page_load_timeout(10)
+driver.get("https://myvisit.com/#!/home/signin/")
+try:
+    iframe = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH,
+                                                                               "//body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[1]/iframe[1]")))
+    print("iframe found")
+    driver.switch_to.frame(iframe)
+    print("iframe switched")
+    element = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/div[1]/div/div/span/div[1]")))
+    print("Element found")
+    element.click()
+except Exception as e:
+    print(e)
+time.sleep(2)
+# Check if recaptcha is ok
+element = driver.find_element(By.XPATH, "/html/body/div[2]/div[3]/div[1]/div/div/span")
+if element.get_attribute("aria-checked") != "true":
+    print("Need verification")
+    try:
+        driver.switch_to.default_content()
+        iframe = driver.find_element(By.XPATH, "/html/body/div[2]/div[4]/iframe")
+        print("iframe found")
+        driver.switch_to.frame(iframe)
+        print("iframe switched")
+        element = driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[2]/div[1]/div[1]/div[2]/button")
+        element.click()
+        time.sleep(1)
+        element = driver.find_element(By.XPATH, "/html/body/div/div/div[7]/a")
+        link = element.get_attribute("href")
+        text = url_to_text(link)
+        element = driver.find_element(By.XPATH, "/html/body/div/div/div[6]/input")
+        element.send_keys(text)
+        time.sleep(1)
+        element = driver.find_element(By.XPATH, "/html/body/div/div/div[8]/div[2]/div[1]/div[2]/button")
+        element.click()
+    except Exception as e:
+        print(e)
+
+driver.switch_to.default_content()
+# press on Continue without account
+try:
+    element = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/a[1]")))
+except :
+    while True:
+        print(driver.execute_script(input("Command:\n")))
+element.click()
+# press on Yes, continue anyway
+element = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[3]/button[1]")))
+element.click()
+# getting the session token
+token = None
+while token is None:
+    time.sleep(0.5)
+    token = driver.execute_script("return sessionStorage['user.session-token'];")
+print(token)
+driver.close()
+print("finished successfully")
+print(timeit.default_timer() - start_time)
